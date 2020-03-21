f3e35de0bfeb90b5e82a3df9cbd467bba21940fe
Hüseyin Kilic <h.kilic@forsit.de>
2020-03-21 13:17:58 +0100

    docker compose

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # illness-webapp
+## Docker-compose usage:
+- `docker-compose up`, exit with ctrl + c
+- `docker-compose up -d` background mode, exit with `docker-compose down`
--- backend/api/api.py
+++ backend/api/api.py
@@ -49,4 +49,4 @@ api.add_resource(IllnessList, '/illness')
 api.add_resource(Illness, '/illness/<string:symptoms>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
