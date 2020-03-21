c2f55ce8a67885750114c28abf90a17dc8b26c95
Aurelius Wendelken <webtobesocial@gmail.com>
2020-03-21 08:30:56 +0100

    added shebang line and file encoding and fixed missing quotes

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 ## flask + mongodb api for illness tracking
 ## Hüseyin Kilic
 ## a lot of help from https://github.com/fblupi/flask-and-mongodb-api-rest
@@ -6,13 +9,12 @@ from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from flask_restful import Resource, Api
 
-app = Flask(__symptoms__)
+app = Flask(__name__)
 api = Api(app)
 
-app.config['MONGO_DBNAME'] = 'illness'
-app.config['MONGO_URI'] = 'mongodb://root:gvOIqn8DJ8tvHz%!@mongodb:27017/?authSource=admin'
+app.config['MONGO_URI'] = 'mongodb://root:gvOIqn8DJ8tvHz%!@localhost:27017/illness?authSource=admin'
 # Disable redirecting on POST method from /illness to /illness/
-app.url_map.strict_slashes = 
+app.url_map.strict_slashes = ''
 
 mongo = PyMongo(app)
 
@@ -40,11 +42,11 @@ class IllnessList(Resource):
         area = request.json['area']
         illness_id = illness.insert({'symptoms': symptoms, 'area': area})
         new_illness = illness.find_one({'_id': illness_id })
-        output = {'symptoms : new_illness['symptoms'], 'area' : new_illness['area']}
+        output = {'symptoms' : new_illness['symptoms'], 'area' : new_illness['area']}
         return jsonify({'result' : output})
 
 api.add_resource(IllnessList, '/illness')
 api.add_resource(Illness, '/illness/<string:symptoms>')
 
-if __symptoms__ == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
