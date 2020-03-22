8f5b8366c95ef5a65d7579c7f47f7cddd19550c7
Bernhard Janetzki <boerni@gmail.com>
2020-03-22 15:17:06 +0100

    force zip_code of 5 chars len

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,6 @@
 from datastore.models import Symptom, Report
+
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 
@@ -16,3 +18,8 @@ class ReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Report
         fields = "__all__"
+
+    def validate_zip_code(self, value):
+        if len(str(value)) != 5:
+            raise serializers.ValidationError(_("ZIP Code must to be 5 chars long"))
+        return value
