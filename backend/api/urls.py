77a8ecf1f60f22e163a78dfc51c87ca30d8af6ad
Bernhard Janetzki <boerni@gmail.com>
2020-03-22 12:23:53 +0100

    report result api endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import SymptomViewSet, ReportViewSet
+from .views import SymptomViewSet, ReportViewSet, ReportResultViewSet
 
 app_name = "api"
 
 urlpatterns = [
     path(r"symptom/", SymptomViewSet.as_view({"get": "list"}), name="symptom"),
     path(r"report/", ReportViewSet.as_view({"post": "create"}), name="report"),
+    path(r"report/result/", ReportResultViewSet.as_view({"get": "retrieve"}), name="report_result"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
-
-from datastore.models import Symptom
+from django.http import JsonResponse
+from rest_framework import viewsets, views
+from django.db.models import F, Count
+from datastore.models import Symptom, Report
 
 from .serializers import SymptomSerializer, ReportSerializer
 
@@ -12,3 +13,15 @@ class SymptomViewSet(viewsets.ReadOnlyModelViewSet):
 
 class ReportViewSet(viewsets.ModelViewSet):
     serializer_class = ReportSerializer
+
+
+class ReportResultViewSet(viewsets.ViewSet):
+    def retrieve(self, *args):
+        results = (
+            Report.objects.values("zip_code_shortened", "symptoms__name")
+            .annotate(users_count_affected=Count("id"))
+            .annotate(zip_code=F("zip_code_shortened"))
+            .annotate(symptom=F("symptoms__name"))
+            .values("zip_code", "users_count_affected", "symptom")
+        )
+        return JsonResponse(list(results), safe=False)
