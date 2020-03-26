bb3aa7fcc56a9701e281fbf4601a52421a37ca35
Konstantin Kulinicenko <konstantin.kulinicenko@gmail.com>
2020-03-26 22:35:54 +0100

    feat(zip_code_filter): fixed

--- frontend/app/pages/results.vue
+++ frontend/app/pages/results.vue
@@ -4,12 +4,10 @@
     <h1 class="text-2xl mb-3 text-blue-600">
       Übersicht der eingereichten Daten
     </h1>
-    <form class="block mb-3">
-      <input type="text" class="w-1/4 border border-blue-600 rounded text-xl p-2 mb-3" v-model="zipCode" placeholder="10115">
-    </form>
+    Filter: <input type="text" class="w-1/4 border border-blue-600 rounded text-xl p-2 mb-3" v-model="zipCode" placeholder="PLZ">
     <div id="map"></div>
     <ul class="list text-blue-600">
-      <li v-for="(item, index) in normalizeResponse(results)" :key="index">
+      <li v-for="(item, index) in results" :key="index">
         <div class="rounded overflow-hidden shadow-lg">
           <div class="px-6 py-4">
             <strong class="text-base">
@@ -35,11 +33,9 @@ import { groupBy } from '../utils';
 export default {
   data() {
     return {
-      map: null,
       results: [],
       zipCode: null,
       zipCodeRegex: /^(?!01000|99999)(0[1-9]\d{3}|[1-9]\d{4})$/,
-      heatmapOverlay: null,
       heatMapLayer: null,
       heatMapConfig: {
         // radius should be small ONLY if scaleRadius is true (or small radius is intended)
@@ -67,43 +63,46 @@ export default {
   },
   async created() {
     try {
-      const results = await this.$axios.$get(`${process.env.API_URL}/report/result/`)
+      await this.getResult()
       const HeatmapOverlay = await require ('@/assets/leaflet-heatmap.js')
       this.heatMapLayer = new HeatmapOverlay(this.heatMapConfig)
-      this.results = results
 
-      this.map = new L.Map('map', {
+      new L.Map('map', {
         dragging: !L.Browser.mobile,
         maxZoom: 10,
         tap: !L.Browser.mobile,
         center: new L.LatLng(51.37328923, 10.21334121),
         zoom: 6,
-        layers: [new L.TileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png')]
+        layers: [new L.TileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png'), this.heatMapLayer]
       })
 
-      this.createMap(results)
+      this.updateMap()
     } catch (e) {
       console.log(e)
     }
   },
   watch: {
-    results: function() {}
-  },
-  watch: {
-    zipCode: function(zipCode) {
-      if (this.zipCodeRegex.test(zipCode)) {
-        console.log(zipCode)
-
-        this.$axios.$get(`${process.env.API_URL}/report/result/?new_format&zip_code=${zipCode}`).then(res => {
-          this.results = res
-          this.updateHeatmap(res)
-        }).catch(e => {
-          console.log(e)
-        })
+    zipCode: async function(zipCode) {
+      try {
+        if (this.zipCodeRegex.test(zipCode) || zipCode === '') {
+          const results = await this.$axios.$get(`${process.env.API_URL}/report/result/${this.zipCodeRegex.test(zipCode) ? `?new_format&zip_code=${zipCode}`: ''}`)
+          this.results = this.normalizeResponse(results)
+          this.updateMap()
+        }
+      } catch (e) {
+        console.log(e)
       }
     }
   },
   methods: {
+    async getResult() {
+      try {
+        const results = await this.$axios.$get(`${process.env.API_URL}/report/result/`)
+        this.results = this.normalizeResponse(results)
+      } catch (e) {
+        console.log(e)
+      }
+    },
     normalizeResponse(response) {
       const groupedResponse = groupBy(response, 'county')
       return Object.keys(groupedResponse)
@@ -130,41 +129,16 @@ export default {
         behavior: 'smooth'
       });
     },
-    async updateHeatmap(response) {
-      this.heatMapLayer.setData({data:[]})
-
-      await this.normalizeResponse(response).forEach(async (item) => {
-        try {
+    updateMap() {
+      this.heatMapStructure.data = []
+      this.results.forEach((item) => {
           this.heatMapStructure.data.push({
             lat: Number(item.latitude),
             lng: Number(item.longitude)
           })
-
-          this.heatMapLayer.setData(this.heatMapStructure)
-        } catch (e) {
-          console.log(e)
-        }
       })
 
-      //this.map.removeLayer(this.heatMapLayer)
-    },
-    async createMap(response) {
-      // eslint-disable-next-line no-undef
-      if (document.getElementById('map')) {
-        await this.normalizeResponse(response).forEach(async (item) => {
-          try {
-            this.heatMapStructure.data.push({
-              lat: Number(item.latitude),
-              lng: Number(item.longitude)
-            })
-            this.heatMapLayer.setData(this.heatMapStructure)
-          } catch (e) {
-            console.log(e);
-          }
-        })
-
-        this.map.addLayer(this.heatMapLayer)
-      }
+      this.heatMapLayer.setData(this.heatMapStructure)
     }
   }
 }
